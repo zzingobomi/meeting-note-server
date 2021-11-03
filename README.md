98c2ce24375ed09461b630bc53fef197772d15c6
zzingobomi <zzingo5@naver.com>
2021-11-03 14:34:37 +0900

    feat: https 서버 기능 추가

--- README.md
+++ README.md
@@ -6,3 +6,16 @@
   2. localtunnel 실행
   - lt --port 4000
   3. 제공되는 URL 로 접속
+
+# CORS
+
+- Credentialed Request
+
+  - chrome은 CORS 요청에 대해 localhost 를 지원하지 않는다.
+  - Access-Control-Allow-Origin에는 \*를 사용할 수 없으며, 명시적인 URL이어야한다.
+  - 응답 헤더에는 반드시 Access-Control-Allow-Credentials: true가 존재해야한다.
+  - 테스트로 127.0.0.1 을 사용한다.
+
+# Synology 인증서
+
+- 인증서 만료일: 2022-01-26
--- src/server.js
+++ src/server.js
@@ -1,16 +1,25 @@
 import http from "http";
 import https from "https";
-import SocketIO from "socket.io";
+import { Server } from "socket.io";
 import express from "express";
+import fs from "fs";
 
 const app = express();
 
+let users = {};
+let socketToRoom = {};
+const maximum = process.env.MAXIMUM || 4;
+
 ///
-/// http 서버
+/// http
 ///
 
+app.get("/", (req, res) => {
+  res.send("hello meeting note server");
+});
+
 const httpServer = http.createServer(app);
-const wsServer = SocketIO(httpServer, {
+const wsServer = new Server(httpServer, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
@@ -20,18 +29,69 @@ const wsServer = SocketIO(httpServer, {
 wsServer.on("connection", (socket) => {
   socket.emit("sayhello", "hello world");
 
-  socket.on("join_room", (roomName) => {
-    socket.join(roomName);
-    socket.to(roomName).emit("welcome");
+  socket.on("join_room", (data) => {
+    if (users[data.room]) {
+      const length = users[data.room].length;
+      if (length === maximum) {
+        socket.to(socket.id).emit("room_full");
+        return;
+      }
+      users[data.room].push({ id: socket.id, nickName: data.nickName });
+    } else {
+      users[data.room] = [{ id: socket.id, nickName: data.nickName }];
+    }
+    socketToRoom[socket.id] = data.room;
+
+    socket.join(data.room);
+    console.log(`[${socketToRoom[socket.id]}]: ${socket.id} enter`);
+
+    const usersInThisRoom = users[data.room].filter(
+      (user) => user.id !== socket.id
+    );
+
+    console.log(usersInThisRoom);
+
+    wsServer.sockets.to(socket.id).emit("all_users", usersInThisRoom);
   });
-  socket.on("offer", (offer, roomName) => {
-    socket.to(roomName).emit("offer", offer);
+
+  socket.on("offer", (data) => {
+    //console.log(data.sdp);
+    socket.to(data.offerReceiveID).emit("getOffer", {
+      sdp: data.sdp,
+      offerSendID: data.offerSendID,
+      offerSendNickName: data.offerSendNickName,
+    });
   });
-  socket.on("answer", (answer, roomName) => {
-    socket.to(roomName).emit("answer", answer);
+
+  socket.on("answer", (data) => {
+    //console.log(data.sdp);
+    socket
+      .to(data.answerReceiveID)
+      .emit("getAnswer", { sdp: data.sdp, answerSendID: data.answerSendID });
   });
-  socket.on("ice", (ice, roomName) => {
-    socket.to(roomName).emit("ice", ice);
+
+  socket.on("candidate", (data) => {
+    //console.log(data.candidate);
+    socket.to(data.candidateReceiveID).emit("getCandidate", {
+      candidate: data.candidate,
+      candidateSendID: data.candidateSendID,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`[${socketToRoom[socket.id]}]: ${socket.id} exit`);
+    const roomID = socketToRoom[socket.id];
+    let room = users[roomID];
+    if (room) {
+      room = room.filter((user) => user.id !== socket.id);
+      users[roomID] = room;
+      if (room.length === 0) {
+        delete users[roomID];
+        return;
+      }
+    }
+    socket.to(roomID).emit("user_exit", { id: socket.id });
+    console.log(users);
   });
 });
 
@@ -39,37 +99,93 @@ const handleListen = () => console.log(`Listening on http://localhost:4000`);
 httpServer.listen(4000, handleListen);
 
 ///
-/// https 서버
+/// https
 ///
 /*
+app.get("/", (req, res) => {
+  res.send("hello meeting note server");
+});
+
 const options = {
-  key: fs.readFileSync("./keys/private.pem"),
-  cert: fs.readFileSync("./keys/public.pem"),
+  key: fs.readFileSync("/volume1/meeting-note-server/keys/privkey.pem"),
+  cert: fs.readFileSync("/volume1/meeting-note-server/keys/cert.pem"),
 };
 
 const httpsServer = https.createServer(options, app);
-const wsServer = SocketIO(httpsServer, {
+const wsServer = new Server(httpsServer, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
+    origin: "https://zzingobomi.github.io",
+    methods: ["GET", "PUT", "POST"],
+    credentials: true
   },
 });
 
 wsServer.on("connection", (socket) => {
   socket.emit("sayhello", "hello world");
 
-  socket.on("join_room", (roomName) => {
-    socket.join(roomName);
-    socket.to(roomName).emit("welcome");
+  socket.on("join_room", (data) => {
+    if (users[data.room]) {
+      const length = users[data.room].length;
+      if (length === maximum) {
+        socket.to(socket.id).emit("room_full");
+        return;
+      }
+      users[data.room].push({ id: socket.id, nickName: data.nickName });
+    } else {
+      users[data.room] = [{ id: socket.id, nickName: data.nickName }];
+    }
+    socketToRoom[socket.id] = data.room;
+
+    socket.join(data.room);
+    console.log(`[${socketToRoom[socket.id]}]: ${socket.id} enter`);
+
+    const usersInThisRoom = users[data.room].filter(
+      (user) => user.id !== socket.id
+    );
+
+    console.log(usersInThisRoom);
+
+    wsServer.sockets.to(socket.id).emit("all_users", usersInThisRoom);
   });
-  socket.on("offer", (offer, roomName) => {
-    socket.to(roomName).emit("offer", offer);
+
+  socket.on("offer", (data) => {
+    //console.log(data.sdp);
+    socket.to(data.offerReceiveID).emit("getOffer", {
+      sdp: data.sdp,
+      offerSendID: data.offerSendID,
+      offerSendNickName: data.offerSendNickName,
+    });
   });
-  socket.on("answer", (answer, roomName) => {
-    socket.to(roomName).emit("answer", answer);
+
+  socket.on("answer", (data) => {
+    //console.log(data.sdp);
+    socket
+      .to(data.answerReceiveID)
+      .emit("getAnswer", { sdp: data.sdp, answerSendID: data.answerSendID });
   });
-  socket.on("ice", (ice, roomName) => {
-    socket.to(roomName).emit("ice", ice);
+
+  socket.on("candidate", (data) => {
+    //console.log(data.candidate);
+    socket.to(data.candidateReceiveID).emit("getCandidate", {
+      candidate: data.candidate,
+      candidateSendID: data.candidateSendID,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`[${socketToRoom[socket.id]}]: ${socket.id} exit`);
+    const roomID = socketToRoom[socket.id];
+    let room = users[roomID];
+    if (room) {
+      room = room.filter((user) => user.id !== socket.id);
+      users[roomID] = room;
+      if (room.length === 0) {
+        delete users[roomID];
+        return;
+      }
+    }
+    socket.to(roomID).emit("user_exit", { id: socket.id });
+    console.log(users);
   });
 });
 
